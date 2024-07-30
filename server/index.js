e304f27e2bcca7609b8181c3b10237d22838c8fc
HoaTran1002 <vanhoa0908751520@gmail.com>
2024-07-30 15:21:01 +0700

    ADD mongoose

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import cors from "cors";
-
+import mongoose from "mongoose";
+import dotnet from "dotenv";
+dotnet.config();
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 4000;
+const atlas_uri = process.env.ATLAS_URI;
 app.use(express.json());
 app.use(cors());
 app.listen(port, (req, res) => {
   console.log(`app listen on port ${port}`);
 });
+mongoose
+  .connect(atlas_uri, {
+    useNewUrlParser: true,
+    useUnifieldTopology: true,
+  })
+  .then(() => {
+    console.log("connect to mongodb success");
+  })
+  .catch((error) => {
+    console.log("falied connect to mongodb:", error);
+  });
